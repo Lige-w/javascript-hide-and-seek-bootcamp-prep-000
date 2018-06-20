c7b56fa8900ed2c87ab8ca0ae9df6eedd2447e66
Lige <lige.williamson@gmail.com>
2018-06-20 12:52:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  const firstSelector = document.querySelector(selector)
+  return firstSelector
+}
+
+function nestedTarget() {
+  const nested = document.getElementById('nested').querySelector('div.target')
+  return nested
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector('#grand-node');
+  var nextNode = node.children[0];
+  while (nextNode) {
+     node = nextNode
+     nextNode = node.children[0]
+   }
+   return node
+}
